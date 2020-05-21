a098c3ea8da7b3a297bce763d9ef496226328285
Perry Ogwuche <perryogwuche1@gmail.com>
2020-05-20 20:03:05 -0400

    modify index file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a docker test react app - PO
+          Working on this docker file - PO
         </p>
         <a
           className="App-link"
